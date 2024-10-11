27c23a558c278bf4d5b382d7b9afff320fbcc2e3
carine-69 <umugabecarine@gmail.com>
2024-10-11 11:25:10 +0200

    set web to listen on 0000 and port 5000

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+# web application listening on 0.0.0.0. and port 5000
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
+    app.url_map.strict_slashes = False
